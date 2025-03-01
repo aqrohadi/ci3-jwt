ab639043c6e4334985928fb63bdbc63fd7b3c464
@imam <imamaqrom@gmail.com>
2025-03-01 14:04:05 +0700

    feat: feature crud

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -11,6 +11,32 @@ class Api extends RestController {
         parent::__construct();
     }
 
+	public function siswa_get(){
+        $this->db->select('*');        
+        $data = array ('data'=>$this->db->get('siswa')->result());        
+        $this->response($data, 200 );
+    }
+
+	public function siswa_post(){
+        $isidata = array('nis'=>$this->input->post('nis'), 'namasiswa'=>$this->input->post('nama'));
+        $this->db->insert('siswa', $isidata);
+        $this->response(array("pesan"=>"berhasil"), 200);
+    }
+
+	public function siswa_put(){                
+        $isidata = array('namasiswa'=>$this->put('nama'));
+        $this->db->where(array('nis'=>$this->put('nis')));
+        $this->db->Update('siswa', $isidata);
+        $this->response(array("pesan"=>"Ubah Data Berhasil"), 200);        
+    }
+
+	public function siswa_delete(){                        
+        $this->db->where('nis', $this->delete('nis'));
+        $this->db->delete('siswa');
+        $this->response(array("pesan"=>"data berhasil dihapus"), 200);        
+    }
+
+	// Example
     public function users_get()
     {
         // Users from a data store e.g. database
