9988e4ab60e168ed61a9f662a6e189564062ca06
@imam <imamaqrom@gmail.com>
2025-03-02 21:15:31 +0700

    feat: jwt with refresh-token

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -27,59 +27,138 @@ class Api extends RestController {
         return $cnf;        
     }
 
-	public function getToken_post(){               
-		$exp = time() + 3600;
-		$token = array(
+	public function getToken_post() {
+		// Generate Access Token
+		$accessTokenExp = time() + 3600; // 1 jam
+		$accessToken = array(
 			"iss" => 'apprestservice',
 			"aud" => 'pengguna',
 			"iat" => time(),
 			"nbf" => time() + 10,
-			"exp" => $exp,
+			"exp" => $accessTokenExp,
 			"data" => array(
 				"username" => $this->input->post('username'),
 				"password" => $this->input->post('password')
 			)
-		);       
-		$jwt = JWT::encode($token, $this->configToken()['secretkey'], 'HS256');
-	$output = [
+		);
+		$accessTokenJWT = JWT::encode($accessToken, $this->configToken()['secretkey'], 'HS256');
+	
+		// Generate Refresh Token
+		$refreshTokenExp = time() + (7 * 24 * 3600); // 7 hari
+		$refreshToken = array(
+			"iss" => 'apprestservice',
+			"aud" => 'pengguna',
+			"iat" => time(),
+			"exp" => $refreshTokenExp,
+			"data" => array(
+				"username" => $this->input->post('username')
+			)
+		);
+		$refreshTokenJWT = JWT::encode($refreshToken, $this->configToken()['secretkey'], 'HS256');
+	
+		// Response
+		$output = [
 			'status' => 200,
 			'message' => 'Berhasil login',
-			"token" => $jwt,                
-			"expireAt" => $token['exp']
-		];      
-	$data = array('kode'=>'200', 'pesan'=>'token', 'data'=>array('token'=>$jwt, 'exp'=>$exp));
-	$this->response($data, 200 );       
-}
+			"access_token" => $accessTokenJWT,
+			"access_token_expire" => $accessTokenExp,
+			"refresh_token" => $refreshTokenJWT,
+			"refresh_token_expire" => $refreshTokenExp
+		];
+	
+		$this->response($output, 200);
+	}
 
-	public function authtoken(){
-        $secret_key = $this->configToken()['secretkey']; 
-        $token = null; 
-        $authHeader = $this->input->request_headers()['Authorization'];  
-        $arr = explode(" ", $authHeader); 
-        $token = $arr[1];        
-        if ($token){
-            try{
+	public function authtoken() {
+		$secret_key = $this->configToken()['secretkey'];
+		$token = null;
+	
+		// Ambil token dari header Authorization
+		$authHeader = $this->input->request_headers()['Authorization'];
+		$arr = explode(" ", $authHeader);
+		$token = $arr[1];
+	
+		if ($token) {
+			try {
+				// Decode token
 				$decoded = JWT::decode($token, new Key($secret_key, 'HS256'));
-                if ($decoded){
-                    return 'benar';
-                }
-            } catch (\Exception $e) {
-                $result = array('pesan'=>'Kode Signature Tidak Sesuai');
-                return 'salah';
-                
-            }
-        }       
-    }
+	
+				// Jika token valid, kembalikan data pengguna
+				if ($decoded) {
+					return $decoded->data; // Data pengguna dari token
+				}
+			} catch (\Exception $e) {
+				// Jika token tidak valid
+				$this->response(['status' => 401, 'message' => 'Token tidak valid atau kedaluwarsa'], 401);
+			}
+		} else {
+			// Jika token tidak ditemukan
+			$this->response(['status' => 400, 'message' => 'Token tidak ditemukan'], 400);
+		}
+	}
 
-	public function siswa_get(){
-        if ($this->authtoken() == 'salah'){
-            return $this->response(array('kode'=>'401', 'pesan'=>'signature tidak sesuai', 'data'=>[]), '401');
-            die();
-        }
-        $this->db->select('*');        
-        $data = array ('data'=>$this->db->get('siswa')->result());        
-        $this->response($data, 200 );
-    }
+	public function refreshToken_post() {
+		$secret_key = $this->configToken()['secretkey'];
+		$refreshToken = $this->input->post('refresh_token');
+	
+		if ($refreshToken) {
+			try {
+				// Decode refresh token
+				$decoded = JWT::decode($refreshToken, new Key($secret_key, 'HS256'));
+	
+				// Periksa apakah refresh token masih valid
+				if ($decoded && $decoded->exp > time()) {
+					// Generate access token baru
+					$accessTokenExp = time() + 3600; // 1 jam
+					$accessToken = array(
+						"iss" => 'apprestservice',
+						"aud" => 'pengguna',
+						"iat" => time(),
+						"nbf" => time() + 10,
+						"exp" => $accessTokenExp,
+						"data" => array(
+							"username" => $decoded->data->username
+						)
+					);
+					$accessTokenJWT = JWT::encode($accessToken, $secret_key, 'HS256');
+	
+					// Response
+					$output = [
+						'status' => 200,
+						'message' => 'Berhasil memperbarui access token',
+						"access_token" => $accessTokenJWT,
+						"access_token_expire" => $accessTokenExp
+					];
+					$this->response($output, 200);
+				} else {
+					$this->response(['status' => 401, 'message' => 'Refresh token expired'], 401);
+				}
+			} catch (\Exception $e) {
+				$this->response(['status' => 401, 'message' => 'Invalid refresh token'], 401);
+			}
+		} else {
+			$this->response(['status' => 400, 'message' => 'Refresh token tidak ditemukan'], 400);
+		}
+	}
+
+	public function siswa_get() {
+		// Validasi access token
+		$authResult = $this->authtoken();
+	
+		if ($authResult === 'salah') {
+			// Jika token tidak valid atau kedaluwarsa
+			return $this->response([
+				'kode' => '401',
+				'pesan' => 'Token tidak valid atau kedaluwarsa',
+				'saran' => 'Gunakan refresh token untuk mendapatkan access token baru'
+			], 401);
+		}
+	
+		// Jika token valid, ambil data siswa
+		$this->db->select('*');
+		$data = array('data' => $this->db->get('siswa')->result());
+		$this->response($data, 200);
+	}
 
 	public function siswa_post(){
         $isidata = array('nis'=>$this->input->post('nis'), 'namasiswa'=>$this->input->post('nama'));
